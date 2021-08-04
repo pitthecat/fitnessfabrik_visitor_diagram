507f44889009fb965535ebe75603dff0db44a678
pitthecat <david.konzack@gmx.de>
2021-08-04 17:23:13 +0200

    improved the script and updated the documentation

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # fitnessfabrik_usage_diagramm
-A project to show visitor statistics of the gym Fitnessfabrik
+
+My local gym Fitnessfabrik started offering a [visitor count](http://besucher.fitnessfabrik.de) during the Corona period in 2020. The websites only shows the current visitor count 
+but it doesn't offer a history function or diagrams. So I created this small project to scrape the data from the website and create useful diagrams out of it. The diagrams offer an easy way to find less busy time spots in the gym :)
+
+<p align="center">
+  <img src="fitness_fabrik_visitor_count.png">
+</p>
+<p align="center">
+     <em>Original website</em>
+</p>
+
+## Script
+
+The Python script uses the BeautifulSoup lib to scrape the visitor count from http://besucher.fitnessfabrik.de every minute. It writes the data to a simple .csv file 
+and uses the pandas and plotly lib to render a HTML diagram from the data.
+
+I run the script on a Raspberry Pi 3.
+
+<p align="center">
+  <img src="sample_count.png">
+</p>
+<p align="center">
+     <em>This a rendered HTML diagram</em>
+</p>
+
+## Trigger
+
+* The script is triggered by a cronjob 
+  * ```50 5 * * * timeout 64800 /usr/bin/python3 /home/pi/fitness/get_count.py```
+  * The cronjob triggers the script at 5:50 CEST and it runs for 64800 s (18 hours). You have to change trigger and run time based on the opening hours of your gym branch.
+    Also keep summer and winter time in mind. 
+* One could also create a [systemd service](https://www.freedesktop.org/software/systemd/man/systemd.service.html) for it and start/stop the systemd service via a cronjob
+
+##  View the diagrams
+
+There multiple options to view the diagrams with the visitor statistics. 
+
+* Local workstation
+  * Go to the output folder specified in the script and open the <ISO_8601_date>.html file (for example 2020-06-30.html)  
+* Remote machine without GUI (for example a Raspberry Pi with CLI only)
+  * A Raspberry Pi runs the script perfectly fine and offers a low power consumption
+  * A good way to view the HTML files is to run a webserver (like Nginx) with directory listing enabled
+  * The script copies the rendered HTML files to the ```/var/www/html/``` directory which can be served via Nginx
+
+<p align="center">
+  <img src="nginx_dir_listing.png">
+</p>
+<p align="center">
+     <em>Nginx hosting the HTML files with directory listing enabled</em>
+</p>
+
+## 
+
+Disclaimer:
+
+I am not associated with the company running the gym. This is just a small hobby project.
--- get_visitor_count.py
+++ get_visitor_count.py
@@ -4,37 +4,39 @@ import datetime
 import csv
 import time
 import os.path
-import boto3
 from bs4 import BeautifulSoup # pip3 install beautifulsoup4
 import pandas as pd # pip3 install pandas
 import plotly.express as px # pip3 install plotly
 import plotly
 import shutil
 
-
 url = 'http://besucher.fitnessfabrik.de/'
-file_name = '/home/pi/fitnessfabrik/{}.csv'.format(datetime.datetime.now().date())
-file_name_plot = '/home/pi/fitnessfabrik/{}.html'.format(datetime.datetime.now().date())
+gym_count_table_position = 2 # my local gym studio (Darmstadt) in the HTML table
+file_name = '/home/pi/fitness/{}.csv'.format(datetime.datetime.now().date())
+file_name_plot = '/home/pi/fitness/{}.html'.format(datetime.datetime.now().date())
 
 # Functions
 def get_current_activity():
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
-    current_count = soup.findAll('td')[2].string.strip()
-    return current_count
+    try:
+        current_count = soup.findAll('td')[gym_count_table_position].string.strip() 
+        return current_count
+    except:
+        print("Error: Couldn't get current count")
 
 def write_to_file():
     current_time = datetime.datetime.now()
     ISO_8601_time = current_time.isoformat()
-    current_count = get_current_activity()
+    try:
+        current_count = get_current_activity()
+        with open(file_name, mode='a') as activity_status:
+            activity_status_writer = csv.writer(activity_status, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            activity_status_writer.writerow([ISO_8601_time, current_count])
+            print(ISO_8601_time, current_count)
+    except:
+        print("Error: Count variable emtpy")
 
-
-    with open(file_name, mode='a') as activity_status:
-        activity_status_writer = csv.writer(activity_status, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-
-        activity_status_writer.writerow([ISO_8601_time, current_count])
-        print(ISO_8601_time, current_count)
- 
 def create_document_for_today():
     with open(file_name.format(file_name), mode='w') as activity_status:
         activity_status_writer = csv.writer(activity_status, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
@@ -42,8 +44,7 @@ def create_document_for_today():
 
 def create_graph():
     plot_file = pd.read_csv(file_name)
-
-    fig = px.line(plot_file, x = 'time', y = 'count', title='Fitness Fabrik Eschollbrücker Besucherzahlen')
+    fig = px.line(plot_file, x = 'time', y = 'count', title='Fitnessfabrik Eschollbrücker Besucherzahlen')
     plotly.offline.plot(fig, filename=file_name_plot)
 
 def copy_graph():
@@ -57,9 +58,10 @@ def run_all():
         time.sleep(60)
 
 # Run
+
 if os.path.isfile(file_name_plot):
-   print("File for today already exists")
-   run_all()
+    print("File for today already exists")
+    run_all()
 else:
     print("No file for today - Creating...")
     create_document_for_today()
